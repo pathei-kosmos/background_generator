2756724e9677d3242cab4d764f5a0a6c3427bd52
pathei-kosmos <63155388+pathei-kosmos@users.noreply.github.com>
2022-03-22 11:35:31 +0100

    strict usage and change var to let

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var body = document.querySelector("body");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var h3 = document.querySelector("h3");
-var button = document.querySelector("button");
+"use strict";
+let body = document.querySelector("body");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let h3 = document.querySelector("h3");
+let button = document.querySelector("button");
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
